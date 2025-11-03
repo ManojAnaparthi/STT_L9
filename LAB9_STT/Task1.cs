2eb43e59d696177eaaa7e78c5b5892544d61435e
ManojAnaparthi <manoj.anaparthi@iitgn.ac.in>
2025-11-03 16:39:47 +0530

    Added all Lab 9 files

---
+++ LAB9_STT/Task1.cs
@@ -0,0 +1,56 @@
+﻿//using System;
+//class Calculator
+//{
+//    public double Add(double a, double b)
+//    {
+//        return a + b;
+//    }
+//    public double Subtract(double a, double b)
+//    {
+//        return a - b;
+//    }
+//    public double Multiply(double a, double b)
+//    {
+//        return a * b;
+//    }
+//    public double Divide(double a, double b)
+//    {
+//        if (b == 0)
+//        {
+//            throw new DivideByZeroException("Division by zero is not allowed.");
+//        }
+//        return (double)a / b;
+//    }
+//}
+
+//class Task1
+//{
+//    static void Main(string[] args)
+//    {
+//        Calculator calculator = new Calculator();
+//        Console.Write("Enter first number: ");
+//        double num1 = Convert.ToDouble(Console.ReadLine());
+//        Console.Write("Enter second number: ");
+//        double num2 = Convert.ToDouble(Console.ReadLine());
+//        try
+//        {
+//            Console.WriteLine($"{num1}+{num2}: {calculator.Add(num1, num2)}");
+//            if (calculator.Add(num1, num2) % 2 == 0)
+//            {
+//                Console.WriteLine("The sum is Even\n");
+//            }
+//            else
+//            {
+//                Console.WriteLine("The sum is Odd\n");
+//            }
+//            Console.WriteLine($"{num1}-{num2}: {calculator.Subtract(num1, num2)}\n");
+//            Console.WriteLine($"{num1}*{num2}: {calculator.Multiply(num1, num2)}\n");
+//            Console.WriteLine($"{num1}/{num2}: {calculator.Divide(num1, num2)}\n");
+//        }
+//        catch (Exception ex)
+//        {
+//            Console.WriteLine($"Error: {ex.Message}");
+//        }
+//        Console.WriteLine();
+//    }
+//}
---
+++ LAB9_STT/Task2.cs
@@ -0,0 +1,36 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace LAB9_STT
+//{
+//    class Task2
+//    {
+//        static double fac(double x)
+//        {
+//            if (x < 0) throw new ArgumentException("Factorial is not defined for negative numbers.");
+//            if (x == 0) return 1;
+//            return x * fac(x - 1);
+//        }
+//        static void Main(string[] args)
+//        {
+//            for (int i = 1; i <= 10; i++) Console.WriteLine($"{i} ");
+//            Console.WriteLine("\n");
+//            foreach (int i in Enumerable.Range(1, 10)) Console.WriteLine($"{i} ");
+//            Console.WriteLine("\n");
+//            string input;
+//            do
+//            {
+//                input = Convert.ToString(Console.ReadLine());
+//            } while (input != "exit");
+//            Console.WriteLine("\n");
+//            Console.WriteLine("Exited");
+//            Console.WriteLine("\n");
+//            Console.WriteLine("Enter a number to compute its factorial:");
+//            double number = Convert.ToDouble(Console.ReadLine());
+//            Console.WriteLine($"Factorial of {number} is {fac(number)}");
+//        }
+//    }
+//}
